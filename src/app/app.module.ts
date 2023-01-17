f121dafbec4d59f699b296381ad3500379638fc2
miraj <miraj.ansari@sysfore.com>
2023-01-17 13:36:29 +0530

    (chore): project structure setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './Components/Common/footer/footer.component';
+import { HeaderComponent } from './Components/Common/header/header.component';
+import { SidemenuComponent } from './Components/Common/sidemenu/sidemenu.component';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
+import { LoginComponent } from './Components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    SidemenuComponent,
+    DashboardComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
