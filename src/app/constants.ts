f144137140f5357cee57c0bed5b32b2b3ae569fd
miraj <miraj.ansari@sysfore.com>
2023-01-18 09:38:50 +0530

    (feat): constants modified

---
+++ src/app/constants.ts
@@ -0,0 +1,17 @@
+//Dev
+
+const ApiEndpoint = ''
+
+
+export const AppSettings = {
+  Urls: {
+    Auth: {
+      login: ApiEndpoint + 'Auth/Token'
+    },
+    Common: {},
+    Master: {
+
+    },
+
+  }
+}
